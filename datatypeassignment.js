b4967052de4f51ac9edf67c695daef492c261e52
Poornima34728 <m.poornimadevi347@gmail.com>
2026-06-09 18:00:57 +0530

    Add files via upload
    Week 1 - Day 2 - Assignment - Datatypes & Varletconst

---
+++ datatypeassignment.js
@@ -0,0 +1,16 @@
+let userName = "Poornima"
+let organization = "EY"
+let contactNumber = 234435
+let knowsAutomation = false
+let usesPlaywright
+
+console.log("User Name: " + userName)
+console.log("Organization:"+organization)
+console.log("Contact Number: " + contactNumber)
+console.log("Knows Automation: " + knowsAutomation) 
+console.log("Uses Playwright: " + usesPlaywright)
+console.log("username :" +typeof userName)
+console.log("Organization :" +typeof organization)
+console.log("Contact Number :" +typeof contactNumber)
+console.log("Knows Automation :" +typeof knowsAutomation)
+console.log("Uses Playwright :" +typeof usesPlaywright)
---
+++ varletconst.js
@@ -0,0 +1,11 @@
+const browserVersion = "Chrome"
+
+function getBrowserVersion() {
+    if(browserVersion == "Chrome") {
+        // var version = "90.0.4430.212"
+        let version = "90.0.4430.212"
+    }
+    console.log("Browser version inside the function:", version)
+
+}
+getBrowserVersion();
